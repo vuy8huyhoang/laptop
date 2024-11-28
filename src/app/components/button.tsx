23b69685e33c23e838409ce25adba2b3f9e2db96
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-11-28 13:03:30 +0700

    ss

--- src/app/components/button.tsx
+++
@@ -1,6 +0,0 @@
-export default function Button(props: { content: string }) {
-    return (
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
-            {props.content}
-        </button>);
-}
