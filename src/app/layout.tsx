f40d9e51bbdf8d9352299aecfe01ce31804bb840
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-11-28 15:14:39 +0700

    ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { SearchProvider } from "./components/searchContext";
-import { FavoriteProvider } from "./components/favoriteContext";//sadasdasdas
-import { AuthProvider } from "./components/authContext";//lllllllllládasdasdasdsdasdasdasd
+import { FavoriteProvider } from "./components/favoriteContext";
+import { AuthProvider } from "./components/authContext";
 
 export const metadata: Metadata = {
   title: "Cửa hàng laptop",
